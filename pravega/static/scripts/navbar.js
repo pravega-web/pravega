074eddcabcdaa8c209cadf08c60da57b6a22efdb
310301 <barbarianarchergoblingiant@gmail.com>
2022-06-30 17:13:49 +0530

    css changes, removed sponser and footer

--- pravega/static/scripts/navbar.js
+++ pravega/static/scripts/navbar.js
@@ -5,7 +5,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-72px";
   }
   prevScrollpos = currentScrollPos;
 }
