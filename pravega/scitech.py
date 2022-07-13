af8e9ac946cc69b474d123416befd9b43606e6f0
deshdeepak <deshdeepaksuthar@outlook.com>
2022-07-13 12:39:51 +0530

    fixed typos added pale logo

--- pravega/scitech.py
+++ pravega/scitech.py
@@ -290,7 +290,7 @@ def register_for_palebluedot():
 
 
         #Checking for duplicate email numbers
-        existing = mycol.find_one({ "participant1_email" : request.form['emailp1'] })
+        existing = mycol.find_one({ "participant1_email" : request.form['email1'] })
 
         paydb = myclient['payments']
         paycol = paydb[event_name]
